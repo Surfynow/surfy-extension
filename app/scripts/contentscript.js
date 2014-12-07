dcfef664dfb3f155d5b3d19b986ead40b7b48f96
Mikhael Harswanto Tanutama <mikhael.harswanto@gmail.com>
2014-12-06 17:07:18 -0800

    Small content script refactoring

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -2,6 +2,58 @@
 
 var surfy = surfy || {};
 
+surfy.init = function () {
+    function addListener() {
+        chrome.runtime.onMessage.addListener(surfy.onClick);
+    }
+
+    console.log("Content script is loaded");
+    if (chrome.runtime.onMessage) {
+        addListener();
+    } else {
+        // sometimes Chrome extension runtime is not ready yet
+        setTimeout(addListener, 500);
+    }
+};
+
+surfy.onClick = function (request, sender, sendResponse) {
+    var currentPageUrl = encodeURIComponent(window.location.origin);
+    console.log(currentPageUrl + Math.random());
+
+    var surfyContainer = surfy.getContainer();
+    if (!surfyContainer.length) {
+        surfy.initContainer(currentPageUrl, sendResponse);
+    } else if (surfyContainer.hasClass('visible')) {
+        surfyContainer.removeClass('visible');
+    } else {
+        surfyContainer.addClass('visible');
+    }
+};
+
+surfy.initContainer = function (currentPageUrl, sendResponse) {
+    var surfyContainer = $("<div id='surfy'/>").addClass('surfyContainer');
+    surfy.appendTemplate(surfyContainer, "container");
+    $("body").append(surfyContainer);
+
+    var content = surfyContainer.find(".content");
+
+    var url = surfy.config.restUrl;
+    $.get(url + "/comments/" + currentPageUrl).done(function (data) {
+        var parentDiv = $("<div/>");
+
+        surfy.appendTemplate(parentDiv, "headerSection", data);
+
+        $.each(data.comments, function (index, comment) {
+            surfy.appendTemplate(parentDiv, "commentSection", comment);
+        });
+
+        content.append(parentDiv);
+        surfy.getContainer().addClass('visible');
+
+        sendResponse({"commentsLoaded": true});
+    });
+};
+
 surfy.getContainer = function () {
     return $("#surfy");
 };
@@ -15,44 +67,5 @@ surfy.appendTemplate = function (container, template, data) {
 };
 
 $(document).ready(function () {
-    var url = surfy.config.restUrl;
-    console.log("Content script is loaded");
-
-    function init(surfyContainer, currentPageUrl, sendResponse) {
-        surfyContainer = $("<div id='surfy'/>").addClass('surfyContainer');
-        surfy.appendTemplate(surfyContainer, "container", {logoUrl: chrome.extension.getURL("images/surfy-logo-alt.png")});
-        $("body").append(surfyContainer);
-
-        var content = surfyContainer.find(".content");
-
-        $.get(url + "/comments/" + currentPageUrl).done(function (data) {
-            var parentDiv = $("<div/>");
-
-            surfy.appendTemplate(parentDiv, "headerSection", data);
-
-            $.each(data.comments, function (index, comment) {
-                surfy.appendTemplate(parentDiv, "commentSection", comment);
-            });
-
-            content.append(parentDiv);
-            surfy.getContainer().addClass('visible');
-
-            sendResponse({"commentsLoaded": true});
-        });
-    }
-
-    chrome.runtime.onMessage.addListener(
-        function (request, sender, sendResponse) {
-            var currentPageUrl = encodeURIComponent(window.location.origin);
-            console.log(currentPageUrl + Math.random());
-
-            var surfyContainer = surfy.getContainer();
-            if (!surfyContainer.length) {
-                init(surfyContainer, currentPageUrl, sendResponse);
-            } else if (surfyContainer.hasClass('visible')) {
-                surfyContainer.removeClass('visible');
-            } else {
-                surfyContainer.addClass('visible');
-            }
-        });
+    surfy.init();
 });
